0e6696b4c6e15409cc2a017f700641402f9fc4cf
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-10-03 16:15:11 -0400

    added async api calls

--- src/server.js
+++ src/server.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const apiKey = process.env.API_KEY_V2;
+const password = process.env.PASSWORD;
+const https = require('https');
+const request = require('request');
+const rp = require('request-promise');
 const morgan = require('morgan');
+
+const url = "https://api.mysportsfeeds.com/v2.0/pull/nfl/2018-regular/week/4/games.json"
+
+//********************************************
+
+const weekFinder = () => {
+	const currentTime = new Date;
+	let currentMonth = currentTime.getMonth() + 1;
+	let currentDate = currentTime.getDate();
+
+}
+
+
+
+//********************************************
+
+const mySportsFeedsApiCall = async (query) => {
+	try {
+		let result = await rp(query)
+	}
+	catch (error){
+		return console.err(error)
+	}
+}
+
+
+
+//********************************************
+
 
 app.use(morgan('combined'))
 
 app.get("/", (req, res) => {
-	res.send("Hello World!")
+	let currentTime = new Date;
+	res.send(currentTime);
 	}
 )
-
-console.log(`Server is listening on ${PORT}`);
+console.log(`Server is listening on localhost:${PORT}`);
 app.listen(PORT)
