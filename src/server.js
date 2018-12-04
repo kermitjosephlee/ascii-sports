3dfb16e2d469ec0462f35ed4c4f9980cf4270886
kermitjosephlee <kermitjosephlee@gmail.com>
2018-12-03 20:09:25 -0500

    pulling api season call, not week call - opening new branch to refactor and clean up

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const https = require('https');
-const url = "https://api.mysportsfeeds.com/v2.0/pull/nfl"
-const request = require('request');
-const rp = require('request-promise');
-const moment = require('moment')
-const morgan = require('morgan');
+const https = require("https");
+const url = "https://api.mysportsfeeds.com/v2.0/pull/nfl";
+const request = require("request");
+const rp = require("request-promise");
+const moment = require("moment");
+const morgan = require("morgan");
 const apiKey = process.env.API_KEY;
 const password = process.env.PASSWORD;
 
@@ -16,93 +16,121 @@ app.set("view engine", "ejs");
 //********************************************
 
 const queryUrlBuilder = url => {
-  const currentNFLYear = moment().get('year')
-  const dayOfWeek = moment().day()
+  const currentNFLYear = moment().get("year");
+  const dayOfWeek = moment().day();
 
   const seasonType = () => {
-    return (moment().get('year') === 2018) ? "regular"
-      : ("playoff")
-  }
+    return moment().get("year") === 2018 ? "regular" : "playoff";
+  };
 
   const weekNumberBuilder = () => {
-    return (currentNFLYear === 2018 && dayOfWeek > 3) ? (moment().get('week') - 35)
-      : (currentNFLYear === 2018 && dayOfWeek <= 3) ? (moment().get('week') - 36)
-      : (currentNFLYear === 2019 && dayOfWeek > 3) ? (moment().get('week') + 1)
-      : (moment().get('week') + 1);
-  }
+    return currentNFLYear === 2018 && dayOfWeek > 3
+      ? moment().get("week") - 35
+      : currentNFLYear === 2018 && dayOfWeek <= 3
+        ? moment().get("week") - 36
+        : currentNFLYear === 2019 && dayOfWeek > 3
+          ? moment().get("week") + 1
+          : moment().get("week") + 1;
+  };
 
   let weekObj = {
-    currentNFLYear: moment().get('year'),
+    currentNFLYear: moment().get("year"),
     seasonType: seasonType(),
-    currentNFLWeek: weekNumberBuilder(),
-  }
-  return (url + `/${weekObj.currentNFLYear}-${weekObj.seasonType}/week/${weekObj.currentNFLWeek}/games.json`)
-}
+    currentNFLWeek: weekNumberBuilder()
+  };
+  return (
+    url +
+    `/${weekObj.currentNFLYear}-${weekObj.seasonType}/week/${
+      weekObj.currentNFLWeek
+    }/games.json`
+  );
+};
 
 //********************************************
 
 const asciiMapper = ({ games, teamsWithByes }) => {
-  games.forEach(gameHandler)
-  console.log(`+----------------------+`)
-  console.log("On Byes: ", teamsWithByesPrinter(teamsWithByes))
+  games.forEach(gameHandler);
+  console.log(`+----------------------+`);
+  console.log("On Byes: ", teamsWithByesPrinter(teamsWithByes));
 };
 
 const nameLengthChecker = name => {
   if (name === "LA") {
-   return "LAR  "
- }
-  return (name.length === 2 ? name + "   " : name + "  ")
-}
+    return "LAR  ";
+  }
+  return name.length === 2 ? name + "   " : name + "  ";
+};
 
 const scoreChecker = score => {
-  return (score === null) ? " 0"
-    : (score.toString().length === 1) ? (" " + score)
-    : score
-}
+  return score === null
+    ? " 0"
+    : score.toString().length === 1
+      ? " " + score
+      : score;
+};
 
-const activeGameChecker = (gameStatus, startTime, currentQuarter, currentQuarterSecondsRemaining) => {
-  return (gameStatus === "UNPLAYED") ? (gameDateMaker(startTime))
-    : (gameStatus === "LIVE") ? (currentQuarter + "Q" + "  " + timeConverter(currentQuarterSecondsRemaining))
-    : "Final"
-}
+const activeGameChecker = (
+  gameStatus,
+  startTime,
+  currentQuarter,
+  currentQuarterSecondsRemaining
+) => {
+  return gameStatus === "UNPLAYED"
+    ? gameDateMaker(startTime)
+    : gameStatus === "LIVE"
+      ? currentQuarter +
+        "Q" +
+        "  " +
+        timeConverter(currentQuarterSecondsRemaining)
+      : "Final";
+};
 
 const gameDateMaker = date => {
-  return moment(date).format('ddd ha').toLowerCase()
-}
+  return moment(date)
+    .format("ddd ha")
+    .toLowerCase();
+};
 
 const downOrdinalMaker = (currentDown, yardsRemaining) => {
-  return (currentDown === 1) ? currentDown + "st & " + yardsRemaining
-    : (currentDown === 2) ? currentDown + "nd & " + yardsRemaining
-    : (currentDown === 3) ? currentDown + "rd & " + yardsRemaining
-    : (currentDown === 4) ? currentDown + "th & " + yardsRemaining
-    : (currentDown === 0) ? "intermission"
-    : ""
-}
+  return currentDown === 1
+    ? currentDown + "st & " + yardsRemaining
+    : currentDown === 2
+      ? currentDown + "nd & " + yardsRemaining
+      : currentDown === 3
+        ? currentDown + "rd & " + yardsRemaining
+        : currentDown === 4
+          ? currentDown + "th & " + yardsRemaining
+          : currentDown === 0
+            ? "intermission"
+            : "";
+};
 
 const timeConverter = currentQuarterSecondsRemaining => {
   const minutes = currentQuarterSecondsRemaining / 60;
   let seconds = currentQuarterSecondsRemaining % 60;
-  (seconds < 10) ? seconds = `0${seconds}` : seconds
-  return `${Math.trunc(minutes)}:${seconds}`
-}
+  seconds < 10 ? (seconds = `0${seconds}`) : seconds;
+  return `${Math.trunc(minutes)}:${seconds}`;
+};
 
 const downAndYardsMaker = (currentDown, yardsRemaining) => {
-  return (currentDown !== null  ? `${downOrdinalMaker(currentDown, yardsRemaining)}` : "")
-}
+  return currentDown !== null
+    ? `${downOrdinalMaker(currentDown, yardsRemaining)}`
+    : "";
+};
 
 const teamsWithByesPrinter = teamsWithByes => {
-  return teamsWithByes.map(x => x.abbreviation).toString()
-}
+  return teamsWithByes ? teamsWithByes.map(x => x.abbreviation).toString() : "";
+};
 
 const possessionAway = (possession, awayTeam) => {
-  const teamPoss = possession && possession.abbreviation
-  return (teamPoss === awayTeam ? String.fromCharCode(187) : " ")
-}
+  const teamPoss = possession && possession.abbreviation;
+  return teamPoss === awayTeam ? String.fromCharCode(187) : " ";
+};
 
 const possessionHome = (possession, homeTeam) => {
-  const teamPoss = possession && possession.abbreviation
-  return (teamPoss === homeTeam ? String.fromCharCode(187) : " ")
-}
+  const teamPoss = possession && possession.abbreviation;
+  return teamPoss === homeTeam ? String.fromCharCode(187) : " ";
+};
 
 const gameHandler = game => {
   const {
@@ -121,41 +149,68 @@ const gameHandler = game => {
       currentYardsRemaining,
       teamInPossession
     }
-  } = game
+  } = game;
 
-  console.log(`+----------------------+`)
-  console.log(" " + possessionAway(teamInPossession, awayTeam) + " " + (nameLengthChecker(awayTeam)) + scoreChecker(awayScoreTotal) + "   " + activeGameChecker(playedStatus, startTime, currentQuarter, currentQuarterSecondsRemaining))
-  console.log(" " + possessionHome(teamInPossession, homeTeam) + " " + (nameLengthChecker(homeTeam)) + scoreChecker(homeScoreTotal) + "   " + downAndYardsMaker(currentDown, currentYardsRemaining))
-}
+  console.log(`+----------------------+`);
+  console.log(
+    " " +
+      possessionAway(teamInPossession, awayTeam) +
+      " " +
+      nameLengthChecker(awayTeam) +
+      scoreChecker(awayScoreTotal) +
+      "   " +
+      activeGameChecker(
+        playedStatus,
+        startTime,
+        currentQuarter,
+        currentQuarterSecondsRemaining
+      )
+  );
+  console.log(
+    " " +
+      possessionHome(teamInPossession, homeTeam) +
+      " " +
+      nameLengthChecker(homeTeam) +
+      scoreChecker(homeScoreTotal) +
+      "   " +
+      downAndYardsMaker(currentDown, currentYardsRemaining)
+  );
+};
 
-let query = rp.get(queryUrlBuilder(url), {
-  "auth": {
-    'user': apiKey,
-    'pass': password,
-    'sendImmediately': true,
+//********************************************
+const testQuery = "https://api.mysportsfeeds.com/v2.0/pull/nfl/2018-regular/games.json"
+
+let query = rp.get(testQuery, {
+  auth: {
+    user: apiKey,
+    pass: password,
+    sendImmediately: true
   }
-})
+});
 
-const mySportsFeedsApiCall = async (query) =>{
+const mySportsFeedsApiCall = async query => {
+
   try {
-    let result = await rp(query)
-    asciiMapper(JSON.parse(result));
-  }
-  catch (e){
-    return console.error("*** ERROR ***",e)
-  }
-}
+    let result = await rp(query);
 
-mySportsFeedsApiCall(query)
+    let storage = JSON.stringify(result.trim())
+    console.log(JSON.parse(storage))
+    asciiMapper(JSON.parse(storage));
+  }
+  catch (e) {
+    return console.error("*** ERROR in Node Server Async Call ***", e);
+  }
+};
+
+mySportsFeedsApiCall(query);
 
 //********************************************
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-	res.send(mySportsFeedsApiCall(query));
-	}
-)
+  res.send(mySportsFeedsApiCall(query));
+});
 
 console.log(`Server is listening on localhost:${PORT}`);
-app.listen(PORT)
+app.listen(PORT);
