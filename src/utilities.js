d29da3ae159cfa16c2d6e4942282c1e973d0f015
kermitjosephlee <kermitjosephlee@gmail.com>
2018-12-06 16:44:15 -0500

    entered in custom team colors to render on results table

--- src/utilities.js
+++ src/utilities.js
@@ -1,11 +1,46 @@
 const format = require("date-fns/format");
-const colors = require("colors");
+const chalk = require("chalk");
 
 const NUM_SUFFIXES = {
   1: "st",
   2: "nd",
   3: "rd",
   4: "th"
+};
+
+const teamColors = {
+  JAX: "#006778",
+  TEN: "#4495D1",
+  NYJ: "#046A38",
+  BUF: "#004FDA",
+  NYG: "#1339aa",
+  WAS: "#ad475f",
+  NO: "#D3BC8D",
+  TB: "#FF7900",
+  NE: "#04328C",
+  MIA: "#008E97",
+  LAR: "#006ede",
+  CHI: "#C83803",
+  IND: "#A2AAAD",
+  HOU: "#A71930",
+  CAR: "#0085CA",
+  CLE: "#FF3C00",
+  BAL: "#5d49da",
+  KC: "#E31837",
+  ATL: "#A71930",
+  GB: "#589787",
+  DEN: "#FB4F14",
+  SF: "#AA0000",
+  CIN: "#FB4F14",
+  LAC: "#0080C6",
+  DET: "#0076B6",
+  ARI: "#97233F",
+  PHI: "#046A38",
+  DAL: "#003594",
+  PIT: "#FFB612",
+  OAK: "#A5ACAF",
+  MIN: "#FFC62F",
+  SEA: "#69BE28"
 };
 
 module.exports = {
@@ -25,9 +60,9 @@ module.exports = {
     return "";
   },
   nameLengthChecker: name => {
-    if (name === "LA") return "LAR ";
-    if (name.length === 2) return name + "  ";
-    return name + " ";
+    if (name === "LA") return chalk.hex(`${teamColors[name]}`).bold(`${name}R`);
+    if (name.length === 2) return chalk.hex(`${teamColors[name]}`).bold(`${name}`) + "  ";
+    return chalk.hex(`${teamColors[name]}`).bold(`${name}`) + " ";
   },
   timeConverter: currentQuarterSecondsRemaining => {
     const minutes = currentQuarterSecondsRemaining / 60;
